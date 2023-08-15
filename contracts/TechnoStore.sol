098b35129e64e092a762a51795c09800109611b4
Georgi Chonkov <georgichonkov@Georgis-MacBook-Pro-2.local>
2023-08-16 00:54:43 +0300

    Refactored 'Product' struct, removed redundant getters and simplified implementation logic

--- contracts/TechnoStore.sol
+++ contracts/TechnoStore.sol
@@ -46,171 +46,116 @@ error Library__ProductNotBought();
 error Library__RefundExpired();
 
 library Library {
-    uint constant AMOUNT = 1;
-
     struct Product {
-        string[] products; // import it outside of the array
-        mapping(string => uint) quantityOfProduct; // ok
-        mapping(string => uint) indexOfProduct; // remove
-        mapping(string => uint) prizeOf;
+        mapping(string => uint) quantityOfProduct;
+        mapping(string => uint) priceOf;
         mapping(string => address[]) buyers;
-        mapping(string => bool) added;
-        mapping(address => string[]) boughtProducts;
-        mapping(address => mapping(string => uint)) boughtAt;
+        // --------------------------------------------------
+        // product: string -> customer: address -> timestamp: uint
+        mapping(string => mapping(address => uint)) boughtAt;
     }
 
     function addProduct(
         Product storage product,
         string calldata _product,
         uint amount,
-        uint prize
+        uint price
     ) public {
-        if (product.added[_product]) {
+        // Check the price(in tokens) - if it is > 0, then the product has already been added
+        if (product.priceOf[_product] > 0) {
             product.quantityOfProduct[_product] += amount;
         } else {
             product.quantityOfProduct[_product] = amount;
-            product.indexOfProduct[_product] = product.products.length;
-            product.prizeOf[_product] = prize;
-            product.added[_product] = true;
-            product.products.push(_product);
+            product.priceOf[_product] = price;
         }
     }
 
     function buyProduct(
         Product storage product,
+        string calldata _product,
         address _customer,
-        uint i,
         IERC20 token
     ) public {
-        string memory _product = product.products[i];
-
         // Check if the store contract(caller) has any allowance in order to use 'transferFrom'
         if (
             token.allowance(msg.sender, address(this)) <
-            product.prizeOf[_product]
+            product.priceOf[_product]
         ) {
             revert Library__CallerHasApprovedInsufficientAmount();
         }
         // Check if there is at least one product
-        if (product.quantityOfProduct[_product] < AMOUNT) {
+        if (product.quantityOfProduct[_product] == 0) {
             revert Library__InsufficientAmount();
         }
         // Check if this customer(msg.sender/tx.origin) has already bought it
-        if (product.boughtAt[_customer][_product] > 0) {
+        if (product.boughtAt[_product][_customer] > 0) {
             revert Library__ProductAlreadyBought();
         }
 
-        // Веднъж един лаптоп
-        product.quantityOfProduct[_product] -= AMOUNT; // -= 1
+        product.quantityOfProduct[_product] -= 1;
         product.buyers[_product].push(msg.sender);
-        product.boughtAt[_customer][_product] = block.number;
-        product.boughtProducts[_customer].push(_product);
+        product.boughtAt[_product][_customer] = block.number;
+
         // TransferFrom:
         token.transferFrom(
             msg.sender,
             address(this),
-            product.prizeOf[_product]
+            product.priceOf[_product]
         );
     }
 
     function refundProduct(
         Product storage product,
+        string calldata _product,
         address _customer,
-        uint i,
         IERC20 token
     ) public {
-        string memory _product = product.products[i];
-
-        // Instead of using another mapping storing if an address has already bought product,
-        // the block.number can be checked and if it is == 0 - revert
-        if (product.boughtAt[_customer][_product] == 0) {
+        // The block.number can be checked and if it is == 0 - revert
+        if (product.boughtAt[_product][_customer] == 0) {
             revert Library__ProductNotBought();
         }
-        if (block.number - product.boughtAt[_customer][_product] > 100) {
+        if (block.number - product.boughtAt[_product][_customer] > 100) {
             revert Library__RefundExpired();
         }
 
-        product.quantityOfProduct[_product] += AMOUNT;
-        uint length = product.buyers[_product].length;
-        product.buyers[_product][i] = product.buyers[_product][length - 1];
-        product.buyers[_product].pop();
-        delete product.boughtAt[_customer][_product]; // reset it back to 0
-        length = product.boughtProducts[_customer].length;
-        product.boughtProducts[_customer][i] = product.boughtProducts[
-            _customer
-        ][length - 1];
-        product.boughtProducts[_customer].pop();
-        // Transfer and refund 80% of the init prize:
-        token.transfer(msg.sender, _refund(product.prizeOf[_product]));
+        product.quantityOfProduct[_product] += 1;
+        delete product.boughtAt[_product][_customer]; // reset timestamp back to 0
+
+        token.transfer(msg.sender, _refund(product.priceOf[_product]));
     }
 
-    // Calculates refund - 80% of the prize of the given product
-    function _refund(uint prize) private pure returns (uint) {
-        return (prize * 4) / 5;
-    }
-
-    function getProduct(
-        Product storage product,
-        uint i
-    ) external view returns (string memory) {
-        return product.products[i];
-    }
-
-    function getAllProducts(
-        Product storage product
-    ) external view returns (string[] memory) {
-        return product.products;
-    }
-
-    function getIndexOf(
-        Product storage product,
-        string memory _product
-    ) external view returns (uint) {
-        return product.indexOfProduct[_product];
+    // Calculates refund - 80% of the price of the given product
+    function _refund(uint price) private pure returns (uint) {
+        return (price * 4) / 5;
     }
 
     function getQuantityOf(
         Product storage product,
-        string memory _product
+        string calldata _product
     ) external view returns (uint) {
         return product.quantityOfProduct[_product];
     }
 
+    function getPriceOf(
+        Product storage product,
+        string calldata _product
+    ) external view returns (uint) {
+        return product.priceOf[_product];
+    }
+
     function getBuyersOf(
         Product storage product,
-        string memory _product
+        string calldata _product
     ) external view returns (address[] memory) {
         return product.buyers[_product];
     }
 
-    function getAmountOfProducts(
-        Product storage product
-    ) external view returns (uint) {
-        return product.products.length;
-    }
-
-    function getAmountOfBoughtProducts(
-        Product storage product,
-        address _customer
-    ) external view returns (uint) {
-        return product.boughtProducts[_customer].length;
-    }
-
-    function boughtProduct(
-        Product storage product,
-        address _customer,
-        uint i
-    ) external view returns (string memory) {
-        return product.boughtProducts[_customer][i];
-    }
-
     function boughtAtTimestamp(
         Product storage product,
-        address _customer,
-        uint i
+        string calldata _product,
+        address _customer
     ) external view returns (uint) {
-        return
-            product.boughtAt[_customer][product.boughtProducts[_customer][i]];
+        return product.boughtAt[_product][_customer];
     }
 }
 
@@ -223,6 +168,7 @@ contract TechnoStore is Ownable {
 
     IERC20 immutable token;
 
+    string[] public products;
     Library.Product product;
 
     constructor(address _token) {
@@ -232,23 +178,24 @@ contract TechnoStore is Ownable {
     function addProduct(
         string calldata _product,
         uint quantity,
-        uint prize
+        uint price
     ) external onlyOwner {
-        product.addProduct(_product, quantity, prize);
+        product.addProduct(_product, quantity, price);
+        products.push(_product);
 
         emit TechnoStore__ProductAdded(_product, quantity);
     }
 
     function buyProduct(uint i) external {
-        product.buyProduct(msg.sender, i, token);
-        string memory _product = product.getProduct(i);
+        string memory _product = products[i];
+        product.buyProduct(_product, msg.sender, token);
 
         emit TechnoStore__ProductBought(_product, msg.sender);
     }
 
     function refundProduct(uint i) external {
-        product.refundProduct(msg.sender, i, token);
-        string memory _product = product.getProduct(i);
+        string memory _product = products[i];
+        product.refundProduct(_product, msg.sender, token);
 
         emit TechnoStore__ProductRefunded(_product, msg.sender);
     }
@@ -259,48 +206,30 @@ contract TechnoStore is Ownable {
 
     */
 
-    function getProduct(uint i) external view returns (string memory) {
-        return product.getProduct(i);
-    }
-
-    function getAllProducts() external view returns (string[] memory) {
-        return product.getAllProducts();
-    }
-
-    function getIndexOf(string memory _product) external view returns (uint) {
-        return product.getIndexOf(_product);
-    }
-
     function getQuantityOf(
-        string memory _product
+        string calldata _product
     ) external view returns (uint) {
         return product.getQuantityOf(_product);
     }
 
+    function getPriceOf(string calldata _product) external view returns (uint) {
+        return product.getPriceOf(_product);
+    }
+
     function getBuyersOf(
-        string memory _product
+        string calldata _product
     ) external view returns (address[] memory) {
         return product.getBuyersOf(_product);
     }
 
     function getAmountOfProducts() external view returns (uint) {
-        return product.getAmountOfProducts();
+        return products.length;
     }
 
-    function getAmountOfBoughtProducts(
+    function boughtAt(
+        string calldata _product,
         address _customer
     ) external view returns (uint) {
-        return product.getAmountOfBoughtProducts(_customer);
-    }
-
-    function boughtProduct(
-        address _customer,
-        uint i
-    ) external view returns (string memory) {
-        return product.boughtProduct(_customer, i);
-    }
-
-    function boughtAt(address _customer, uint i) external view returns (uint) {
-        return product.boughtAtTimestamp(_customer, i);
+        return product.boughtAtTimestamp(_product, _customer);
     }
 }
