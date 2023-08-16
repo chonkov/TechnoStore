083d04c088c66e9d7c336c96937c8aeb2b295c1e
Georgi Chonkov <georgichonkov@Georgis-MacBook-Pro-2.local>
2023-08-16 13:18:53 +0300

    Updated token contract from a pure 'ERC20' to 'ERC20Permit'

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -1,10 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
-contract Token is ERC20 {
-    constructor() ERC20("LimeTechno Store Token", "LTTK") {
+contract Token is ERC20Permit {
+    constructor()
+        ERC20Permit("LimeTechno Store")
+        ERC20("LimeTechno Store Token", "LTTK")
+    {
         _mint(_msgSender(), 10000);
     }
 }
