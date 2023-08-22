3a3ddaffc3aaf7eafcc75269bdde9aa570e6c3e6
Georgi Chonkov <georgichonkov@Georgis-MacBook-Pro-2.local>
2023-08-22 19:48:35 +0300

    Removed redundant 'console.log' import and state var

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -2,15 +2,8 @@
 pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
-import "hardhat/console.sol";
 
 contract Token is ERC20Permit {
-    // Needed for computing the correct hash. Declared here just as in 'ERC20Permit', because it's private
-    bytes32 private constant _PERMIT_TYPEHASH =
-        keccak256(
-            "Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)"
-        );
-
     constructor()
         ERC20Permit("LimeTechno Store Token")
         ERC20("LimeTechno Store Token", "LTSK")
