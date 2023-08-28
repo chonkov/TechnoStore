d04137b4c314898c7a52b5dd2b7b68b69041864d
Georgi Chonkov <georgichonkov@Georgis-MacBook-Pro-2.local>
2023-08-28 16:17:04 +0300

    Created a custom task for deploying the 'TechnoStore' contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,6 +4,27 @@ require("dotenv").config();
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "";
 const PRIVATE_KEY = process.env.PRIVATE_KEY || "0x";
 
+task("deploy", "Deploys 'TechnoStore' contract")
+  .addPositionalParam("tokenAddr")
+  .addPositionalParam("libraryAddr")
+  .setAction(async (taskArgs, hre) => {
+    const tokenAddr = taskArgs["tokenAddr"];
+    const libraryAddr = taskArgs["libraryAddr"];
+
+    const Token = await hre.ethers.getContractFactory("Token");
+    const token = Token.attach(tokenAddr);
+
+    const TechnoStore = await ethers.getContractFactory("TechnoStore", {
+      libraries: {
+        Library: libraryAddr,
+      },
+    });
+    const technoStore = await TechnoStore.deploy(token.target);
+    await technoStore.waitForDeployment();
+
+    console.log(`Techno Store address: ${technoStore.target}`);
+  });
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
