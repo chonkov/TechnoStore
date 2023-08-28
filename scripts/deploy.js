f370ddbf32a5574cabc2e4872c4f990d51c97924
Georgi Chonkov <georgichonkov@Georgis-MacBook-Pro-2.local>
2023-08-28 11:51:39 +0300

    Added deployment scripts for the 'Token' & 'TechnoStore' contracts

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,32 +1,25 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const { ethers } = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const Token = await ethers.getContractFactory("Token");
+  const token = await Token.deploy();
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+  console.log(`Token address: ${token.target}`);
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
+  const Library = await ethers.getContractFactory("Library");
+  const library = await Library.deploy();
+  console.log(`Library address: ${library.target}`);
+
+  const TechnoStore = await ethers.getContractFactory("TechnoStore", {
+    libraries: {
+      Library: library.target,
+    },
   });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  const technoStore = await TechnoStore.deploy(token.target);
+  await technoStore.waitForDeployment();
+  console.log(`Techno Store address: ${technoStore.target}`);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
